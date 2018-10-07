9f89e411f73a7c311f05d4907358ee60c8204b65
sporff <sporff@gmail.com>
2018-10-06 22:43:28 -0400

    something

--- js/indexjs.js
+++ js/indexjs.js
@@ -4,20 +4,27 @@
 // Generate an empty graph
 function buildTimelineGraph(parentDivID, newGraphDivID, graphStruct)
 {
+    var graphWrapperID = 'graphwrapper_' + newGraphDivID;
+    var rowLegendName = 'graphrowlegend_' + newGraphDivID;
     var parentDiv = $('#' + parentDivID);
     if (parentDiv.length === 0) {
         console.log("Error: Cannot find parent div!");
         return;
     }
     // Erase anything from parent and create new div inside
-    parentDiv.html("");
+    parentDiv.html('');
 
     // Add the graph's main div
-    parentDiv.append('<div id="graphwrapper_' + newGraphDivID + '" style=""></div>');
-    var graphWrapperDiv = $('#graphwrapper_' + newGraphDivID);
-    graphWrapperDiv.append('<div id="graphrowlegend_' + newGraphDivID + '" style="text-align:right;float: left; display:inline-block;background-color:#eee;">Work<br>Conveyor View<br>Missed Window<br>Zone Interruption<br>Unknown Container<br>something<br>something else</div>');
+    parentDiv.append('<div id="' + graphWrapperID + '" style="width=100%;"></div>');
+    var graphWrapperDiv = $('#' + graphWrapperID);
+    //var rowString = '<div id="'+ newGraphDivID +'_header" class="timelineRow timelineRowHeader">jkl</div>';
+    //newGraphDiv.append(rowString);
+
+    //graphWrapperDiv.append('<div id="' + rowLegendName + '" style="text-align:center;float: left; display:inline-block;background-color:#eee;">Work<br>Conveyor View<br>Missed Window<br>Zone Interruption<br>Unknown Container<br>something<br>something else</div>');
+    graphWrapperDiv.append('<div id="' + rowLegendName + '" style="text-align:center;float: left; border: 1px solid black; border-radius: 5px; border-right: 0px;display:inline-block;background-color:#eee;"></div>');
     graphWrapperDiv.append('<div id="' + newGraphDivID + '" style="display:inline-block;width:80%;margin: 0 auto; border: 1px solid black; border-radius: 5px;"></div>');
     var newGraphDiv = $('#' + newGraphDivID);
+    var legendDiv = $('#' + rowLegendName);
 
     // Save any data that will be needed later
     newGraphDiv.data( {
@@ -29,11 +36,6 @@ function buildTimelineGraph(parentDivID, newGraphDivID, graphStruct)
         onmouseover: graphStruct.onmouseover,
         onmouseleave: graphStruct.onmouseleave,
     } );
-
-    // Add header
-    var rowString = '<div id="'+ newGraphDivID +'_header" class="timelineRow timelineRowHeader"></div>';
-    //newGraphDiv.append(rowString);
-    console.log(rowString);
     
 
     for (r=0; r<graphStruct.rowStruct.length; r++)
@@ -52,13 +54,31 @@ function buildTimelineGraph(parentDivID, newGraphDivID, graphStruct)
         
         // Add the html
         var rowString = '<div id="'+ rowID +'" class="timelineRow '+ rowClass +'"';
-        rowString += ' style="'
+        rowString += ' style="';
+        rowString += ' width: 100%;';
         rowString += ' height: ' + rowHeight + ';';
         rowString += ' line-height: ' + rowHeight + ';';
         if (graphStruct.rowStruct[r].rowColor != null)
-            rowString += ' background-color: ' + graphStruct.rowStruct[r].rowColor + ';"'
+            rowString += ' background-color: ' + graphStruct.rowStruct[r].rowColor + ';'
         rowString += '"></div>';
         newGraphDiv.append(rowString);
+
+        var legendRowID = 'legendrow_' + rowID;
+        var legendRowString = '<div id="' + legendRowID +'" class="timelineRow '+ rowClass +'"';
+        legendRowString += ' style="';
+        rowString += ' width: 100%;';
+        legendRowString += ' height: ' + rowHeight + ';';
+        legendRowString += ' line-height: ' + rowHeight + ';';        
+        if (graphStruct.rowStruct[r].rowColor != null)
+            legendRowString += ' background-color: ' + graphStruct.rowStruct[r].rowColor + ';'
+        legendRowString += '">'
+        if (graphStruct.rowStruct[r].title != null)
+            legendRowString += graphStruct.rowStruct[r].title;
+        legendRowString += '</div>';
+        console.log(legendRowString);
+        
+        legendDiv.append(legendRowString);
+
 
         //console.log(rowString);
 
@@ -75,7 +95,7 @@ function addTimelineBars(graphID, rowID, barData)
     var farthestEnd = 0;
     var overlapHeightTracker = 80;
     var opacityTracker = 1;
-    if (rowDiv.length === 0 || graphDiv === 0) {
+    if (rowDiv.length === 0 || graphDiv.length === 0) {
         console.log("Error: Cannot find graph or row!");
         return;
     }
@@ -86,12 +106,12 @@ function addTimelineBars(graphID, rowID, barData)
         var barEnd = barData[b].end;
         var barLen = barEnd - barStart;
 
-        if (barEnd < barStart) {
+        if (barEnd < barStart) {                        // If start/end are reversed, swap them
             [barStart, barEnd] = [barEnd, barStart];
             barLen = -barLen;
         }
-        if (barEnd <= 0 || barStart >= 100)
-            return;
+        if (barEnd <= 0 || barStart >= 100)     // If bar isn't inside of timeline, don't add it
+            continue;
 
         if (barStart < farthestEnd) {
             overlapHeightTracker -= 10;
@@ -124,7 +144,7 @@ function addTimelineBars(graphID, rowID, barData)
             //barString += ' onmouseleave="'+ onmouseleave +'()"';
         }
         // style
-        barString += ' style="opacity: '+ opacityTracker +';top: 10%; height: ' + overlapHeightTracker + '%; width: '+ barLen +'%; margin: 0 '+ barStart +'%; background-color: '+ barColor +';';
+        barString += ' style="overflow:hidden; opacity: '+ opacityTracker +';top: 10%; height: ' + overlapHeightTracker + '%; width: '+ barLen +'%; margin: 0 '+ barStart +'%; background-color: '+ barColor +';';
         //barString += ' style="height:80%; width: '+ barLen +'%; margin: 0 '+ barStart +'%; background-color: '+ barColor +';';
         /* if (graphShadow === true) {
             barString += ' box-shadow: 1px 1px;';                // limited to 1px shadow for now
@@ -142,6 +162,8 @@ function addTimelineBars(graphID, rowID, barData)
         }
 
         barString += '">';
+        //if (barData[b].title != null)
+        //    barString += barData[b].title;
         barString += '</div>';
         //console.log(barString);
         rowDiv.append(barString);
@@ -166,12 +188,13 @@ function mouseOverCallback(graphID, rowID, barID)
 function mouseLeaveCallback(graphID, rowID, barID)
 {
     var infoPane = $('#infoPane');
-    infoPane.html("");
+    infoPane.html('');
     //infoPane.html("leave: " + graphID , rowID , barID);
     //console.log("leave: " + graphID , rowID , barID);
 }
 
-$(document).ready(function(){
+//$(document).ready(function(){
+require(['dojo/domReady!'], function() {
     console.log("Ready");
 
     $('#subPage').load('pages/subpage1.html');
@@ -187,12 +210,12 @@ $(document).ready(function(){
         onmouseover: "mouseOverCallback",
         onmouseleave: "mouseLeaveCallback",
         rowStruct: [
-            {id:"row1", title: "row title", rowColor:"#aaaaaa", barColor:"#3d3"},
-            {id:"row2", barColor:"#33d"},
-            {id:"row3", barColor:"#dd3"},
-            {id:"row4", barColor:"#d33"},
-            {id:"row5", barColor:"#3ad"},
-            {id:"row6", barColor:"#33F"},
+            {id:"row1", title: "Work", rowColor:"#aaaaaa", barColor:"#3d3"},
+            {id:"row2", title: "WUI Exception View", barColor:"#33d"},
+            {id:"row3", title: "Missed Window", barColor:"#dd3"},
+            {id:"row4", title: "Unknown Ctn", barColor:"#3ac"},
+            {id:"row5", title: "Duplicate Scan", barColor:"#d33"},
+            {id:"row6", title: "Interrupted Zone", barColor:"#33F"},
             {id:"row7", barColor:"#dd3"},
             {id:"row8", rowColor:"#555555", barColor:"#d33"},
         ]
@@ -201,7 +224,7 @@ $(document).ready(function(){
     buildTimelineGraph( 'main', 'mainGraph', graphStructure );
 
     row1data = [
-        { id:"bar0", title:"[BWS02 - Zone 3]", start:5, end:17 },
+        { id:"bar0", title:"[BWS02 - Zone 3]sldldsjflskjflskjfldskfj", start:-15, end:17 },
         { id:"bar1", start:21, end:41 },
         { id:"bar2", start:37, end:65 },
         { id:"bar2", start:55, end:60 },
@@ -211,7 +234,7 @@ $(document).ready(function(){
         { id:"2bar0", start:10, end:20, barColor:"#ff00ff", },
         { id:"2bar1", start:30, end:35 },
         { id:"2bar2", start:50, end:70 },
-        { id:"2bar3", start:80, end:93 },
+        { id:"2bar3", start:80, end:93, barColor:"#fa4"},
     ];
     row3data = [
         { id:"3bar0", start:5, end:95 },
@@ -232,3 +255,5 @@ $(document).ready(function(){
 
     console.log("Done");
 });
+
+
